5d83e02a6d6de5c850f05e156b6c677f597cf084
Boncho Belutov <composer2be@gmai.com>
2017-12-17 19:08:57 +0200

    added initial structure for app login

--- server/config/express.js
+++ server/config/express.js
@@ -5,21 +5,62 @@ const express = require('express'),
     cookieParser = require('cookie-parser'),
     bodyParser = require('body-parser'),
     path = require('path'),
-    hbs = require('express-handlebars');
+    hbs = require('express-handlebars'),
+    expressValidator = require('express-validator'),
+    flash = require('connect-flash'),
+    passport = require('passport');
 
 
 let app = express();
+// View Engine
 app.engine('hbs', hbs({ extname: 'hbs', defaultLayout: 'layout', layoutsDir: 'server/views/layouts/' }));
 app.set('view engine', 'hbs');
 app.set('views', './server/views');
+// Set Static Folder
 app.use('/res', express.static('public'));
-app.use(cookieParser());
+// BodyParser Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+// Express Session
 app.use(expressSession({
     secret: 'SimpleSyrop',
     resave: true,
     saveUninitialized: true
 }));
+// Passport init
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Express Validator
+app.use(expressValidator({
+    errorFormatter: function(param, msg, value) {
+        var namespace = param.split('.'),
+            root = namespace.shift(),
+            formParam = root;
+
+        while (namespace.length) {
+            formParam += '[' + namespace.shift() + ']';
+        }
+        return {
+            param: formParam,
+            msg: msg,
+            value: value
+        };
+    }
+}));
+
+// Connect Flash
+app.use(flash());
+
+// Global Vars
+app.use(function(req, res, next) {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    res.locals.user = req.user || null;
+    next();
+});
+
 
 module.exports = app;
